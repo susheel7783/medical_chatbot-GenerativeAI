6ec654ccd999f64003faa9bc1b0cbcadf014c130
Susheel Kumar <susheelkumar7783@gmail.com>
2024-12-15 11:25:11 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Susheel Kumar',
+    author_email='susheelkumar7783@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
